748f40cf064dfbd6e8b7b807fd6b8e46b7b0fcf8
David Jiang <davidjgrey@gmail>
2016-08-22 09:11:01 -0700

    preparing to deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,18 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+ Paperclip.options[:command_path] = "/usr/local/bin"
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+    :s3_host_name => "s3-us-west-2.amazonaws.com",
+    :s3_endpoint => "s3-us-west-2.amazonaws.com",
+    :bucket => Rails.application.secrets.aws_bucket,
+    :access_key_id => Rails.application.secrets.aws_id,
+    :secret_access_key => Rails.application.secrets.aws_secret_key,
+    :s3_region=> Rails.application.secrets.s3_region
+    }
+  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
